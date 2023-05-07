9203ecc61005fb988c53c475c88568ceaba4d331
Daison <daisonthms@gmail.com>
2023-05-07 22:45:02 +0530

    .

--- app/[address]/page.tsx
+++ app/[address]/page.tsx
@@ -21,41 +21,34 @@ export const metadata: Metadata = {
 export default function DashboardPage({ params }: any) {
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/dashboard-light.png"
-          width={1280}
-          height={866}
-          alt="Dashboard"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/dashboard-dark.png"
-          width={1280}
-          height={866}
-          alt="Dashboard"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="hidden flex-col md:flex">
-        <div className="border-b">
+      <div className="flex-col md:flex">
+        <div className="border-b border-black">
           <div className="flex h-16 items-center px-4">
-            <TeamSwitcher />
-            <MainNav className="mx-6" />
-            <div className="ml-auto flex items-center space-x-4">
+            <div className="mx-auto flex items-center space-x-4">
+              <Image className="hidden sm:inline-flex" src="/soldash_logo.png" width="200" height="50" alt="logo" />
               <Search />
-              <UserNav />
+              <Button size="sm">
+                <Download className="mr-2 h-4 w-4" />
+                Connect
+              </Button>
             </div>
           </div>
         </div>
-        <div className="flex-1 space-y-4 p-8 pt-6">
+        <div className="flex-1 space-y-4 p-8 pt-6 xl:mx-64">
           <div className="flex items-center justify-between space-y-2">
             <h2 className="text-3xl font-bold tracking-tight">Dashboard {params.address}</h2>
             <div className="flex items-center space-x-2">
-              <CalendarDateRangePicker />
               <Button size="sm">
                 <Download className="mr-2 h-4 w-4" />
-                Download
+                Connect
+              </Button>
+              <Button size="sm">
+                <Download className="mr-2 h-4 w-4" />
+                Connect
+              </Button>
+              <Button size="sm">
+                <Download className="mr-2 h-4 w-4" />
+                Connect
               </Button>
             </div>
           </div>
@@ -127,7 +120,7 @@ export default function DashboardPage({ params }: any) {
                   </CardContent>
                 </Card>
               </div>
-              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-6">
                 <Card className="col-span-3">
                   <CardHeader>
                     <CardTitle>Recent Sales</CardTitle>
