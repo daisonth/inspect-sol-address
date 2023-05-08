a8365b65e18fa97d81a10a45b17bfc27c5f0d8d5
Daison <daisonthms@gmail.com>
2023-05-08 16:45:17 +0530

    .

--- app/[address]/page.tsx
+++ app/[address]/page.tsx
@@ -4,14 +4,40 @@ import { RecentSales } from "./components/recent-sales"
 import HeadTitle from "./components/headTitle"
 import TabSwitcher from "./components/TabSwitcher"
 import DashCard from "./components/DashCards"
+import axios from "axios"
+import { Metaplex } from "@metaplex-foundation/js"
+import { Connection, clusterApiUrl, PublicKey } from "@solana/web3.js"
 
-const getData = async () => {
-  const res = await fetch("https://api.cms.com/....");
-  return res.json();
-};
+const url = "https://rest-api.hellomoon.io/v0/token/balances-by-owner";
 
+async function TokenBalancesByOwner(add: string) {
+  const connection = new Connection(clusterApiUrl("mainnet-beta"));
+  const metaplex = new Metaplex(connection);
 
-export default function DashboardPage(params: any) {
+  const { data } = await axios.post(
+    url,
+    {
+      ownerAccount: add
+    },
+    {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer 014b8edb-ca22-4142-bbd8-58fa9e33ce95",
+      },
+    }
+  );
+
+  const addr = data[0].mint;
+  const mintad = new PublicKey(addr);
+  const nft = await metaplex.nfts().findByMint({ mintAddress: mintad });
+
+  return data;
+}
+
+export default async function DashboardPage({ params }: any) {
+  const data = await TokenBalancesByOwner(params.address)
+
   return (
     <div className="flex-col md:flex">
       <div className="flex-1 space-y-4 p-8 pt-6 xl:mx-64">
@@ -31,7 +57,7 @@ export default function DashboardPage(params: any) {
 
               <Card className="col-span-3">
                 <CardHeader>
-                  <CardTitle>Recent Sales</CardTitle>
+                  <CardTitle>Fundible Tockens</CardTitle>
                   <CardDescription> You made 265 sales this month. </CardDescription>
                 </CardHeader>
                 <CardContent> <RecentSales /> </CardContent>
@@ -39,7 +65,7 @@ export default function DashboardPage(params: any) {
 
               <Card className="col-span-3">
                 <CardHeader>
-                  <CardTitle>Recent Sales</CardTitle>
+                  <CardTitle>Non Fundible Tokens</CardTitle>
                   <CardDescription> You made 265 sales this month. </CardDescription>
                 </CardHeader>
                 <CardContent> <RecentSales /> </CardContent>
