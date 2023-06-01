6d0ffc660d81895eb983cbf43f222057a7ee63c5
Daison <daisonthms@gmail.com>
2023-06-01 18:10:49 +0530

    updated landing page and logp

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,20 +1,13 @@
 import Image from "next/image"
 import { Search } from "./search"
 import WalletConnector from "./WalletConnector"
-import Link from 'next/link'
 
-function NavBar() {
+export default function NavBar() {
   return (
-    // <div className="fixed inset-x-0 top-0 z-50 border-b border-black">
-    // <div className="flex h-16 items-center px-4">
     <div className="sticky top-0 z-50 mx-auto h-16 md:px-32 flex justify-between items-center md:gap-20 md:space-x-4 border-b border-black backdrop-blur ">
-      <a href="/"><Image className="hidden sm:inline-flex" src="/soldash_logo.png" width="200" height="50" alt="logo" /></a>
+      <a href="/"><Image className="hidden sm:inline-flex" src="/soldash_dark.png" width="200" height="50" alt="logo" /></a>
       <Search />
       <WalletConnector />
     </div>
-    // </div >
-    // </div>
   )
 }
-
-export default NavBar;
